1818f3f7d876cc677afc484842a4dd83598863b2
Williams Espinoza <spinza6006@gmail.com>
2022-07-13 12:11:34 +0200

    user_dao_Test test folder

--- tests/unit/users_dao_test.py
+++ tests/unit/users_dao_test.py
@@ -1,11 +1,13 @@
 import unittest
 
-from users_dao import users_dao
-#from project.persistence.users_dao import get_users
 from project.persistence.users_dao import *
 
 
 class User_dao_Test(unittest.TestCase):
-    def get_user(self,user_id):
-        user = get_user(id)
+    def test_get_user(self):
+        user = get_user(user_id=2)
         print(user)
+        self.assertEqual(user["id"],2)
+        self.assertIsNotNone(user["fullname"])
+
+    
