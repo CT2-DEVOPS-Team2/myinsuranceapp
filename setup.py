c3b681bf44a3948b61fe5df57c5959d20357200e
Duygu Yildiz <duygu.gurbuz@netmind.es>
2022-07-13 22:08:38 +0200

    jenkins tests added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyInsuranceApp',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/CT2-DEVOPS-Team2/myinsuranceapp',
+    author='Ipanema',
+    author_email='myemail@example.com'
+)
