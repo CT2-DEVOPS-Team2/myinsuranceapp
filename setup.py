3a3357514e6714357d8bb685d00c8d01e3fd2bf4
Duygu Yildiz <duygu.gurbuz@netmind.es>
2022-07-14 00:25:35 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='MyInsuranceApp',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
     long_description=open('README.md').read(),
-    install_requires=['numpy'],
     url='https://github.com/CT2-DEVOPS-Team2/myinsuranceapp',
     author='Ipanema',
     author_email='myemail@example.com'
