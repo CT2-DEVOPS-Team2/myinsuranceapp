8dc57106d87f5db877a7852a68196ae1905ce6fc
Mazzie <mazzieola@hotmail.com>
2022-07-13 13:26:52 +0200

    post

--- tests/acceptancetest/test_acceptance_user.py
+++ tests/acceptancetest/test_acceptance_user.py
@@ -6,35 +6,8 @@ import project
 
 class TestApp(unittest.TestCase):
     token=''
-
-    # def test_1_get_(self):
-    #     route = project.login(self)
-    #     response = route.get('/api/v1/customers/', content_type='application/json')
-    #     data=json(response.text)
-    #     print(f"get 1: {data}")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(data, [])
-
-    # def test_2_post(self):
-    #     tester = project.test_client(self)
-    #     test_data = {"address": "5 Spenser Plaza",
-    #                  "email": "kbraksper0@mac.com",
-    #                  "name": "Kelbee"}
-    #     response = tester.post('/api/v1/customers/',content_type='application/json', json=test_data)
-    #     data=json.loads(response.text)
-    #     print(f"post: {data}")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTrue(data['id'] != None)
-
-    # def test_3_get_none(self):
-    #     tester = project.test_client(self)
-    #     response = tester.get('/api/v1/customers/', content_type='application/json')
-    #     data=json.loads(response.text)
-    #     print(f"get 2: {data}")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTrue(len(data)>0)
-
-    def test_4_testToken(self):
+    
+    def test_1_testToken(self):
         tester = app.test_client(self)
         test_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
         response = tester.post('/api/v1/token',content_type='application/json', json=test_data)        
@@ -44,7 +17,7 @@ class TestApp(unittest.TestCase):
         if response.status_code==200:
             TestApp.token=data['token']
         
-    def test_5_get_restricted(self):
+    def test_2_get_restricted(self):
         tester = app.test_client(self)
         print(f"token: {self.token}")
         headers = {"Authorization": f"Bearer {TestApp.token}"}
---
+++ tests/acceptancetest/test_acceptance_userpost.py
@@ -0,0 +1,35 @@
+import json
+import unittest
+from project import app
+import project
+
+
+class TestApp(unittest.TestCase):
+    token=''
+
+    def test_1_post_(self):
+        route = app.test_client(self)
+        response = route.post('/api/v1/users/', content_type='application/json')
+        data=json(response.text)
+        print(f"get post: {data}")
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(data, [0])
+
+    def test_2_post(self):
+        tester = app.test_client(self)
+        test_data = {"address": "C Street 1",
+                     "email": "jd@myinsuranceapp.com",
+                     "fullname": "John Doe"}
+        response = tester.post('/api/v1/users/',content_type='application/json', json=test_data)
+        data=json.loads(response.text)
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data[0]['id'])
+
+    # def test_3_get_none(self):
+    #     tester = app.test_client(self)
+    #     response = tester.post('/api/v1/token', content_type='application/json')
+    #     data=json.loads(response.text)
+    #     print(f"get post 2: {data}")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTrue(len(data)>0)
