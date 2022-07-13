81ad9c15be6bdb12b4966f088131a127fe05ae77
Williams Espinoza <spinza6006@gmail.com>
2022-07-13 12:33:48 +0200

    updating unittest users_dao_test

--- tests/unit/users_dao_test.py
+++ tests/unit/users_dao_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from project.persistence.users_dao import *
-
+from flask import jsonify, request
 
 class User_dao_Test(unittest.TestCase):
     def test_get_user(self):
@@ -9,5 +9,8 @@ class User_dao_Test(unittest.TestCase):
         print(user)
         self.assertEqual(user["id"],2)
         self.assertIsNotNone(user["fullname"])
-
     
+    def test_edit_user(self):
+        #user = request.json
+        ok = edit_user("williams", "w@.es", "13-08", "spain", "london", "street1", "12345", 2)
+        
